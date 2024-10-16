6391f2bdafb206754e84ee7fa22c56ed656d7aec
GabrielGC <goncalves.cunhaa@gmail.com>
2024-10-16 16:17:19 -0300

    feat: adiciona registro das cartas

---
+++ src/constants/cartas.js
@@ -0,0 +1,20 @@
+const imagens = [
+  "carta-1.jpg",
+  "carta-2.jpg",
+  "carta-3.jpg",
+  "carta-4.jpg",
+  "carta-5.jpg",
+  "carta-6.jpg",
+];
+
+const cartasUnicas = imagens.map((imagem, idDoPar) => ({
+  idDoPar,
+  imagem,
+}));
+
+export const paresDeCartas = [...cartasUnicas, ...cartasUnicas].map(
+  (carta, id) => ({
+    ...carta,
+    id,
+  })
+);
