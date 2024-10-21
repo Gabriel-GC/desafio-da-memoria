59e6626e0bc3170fe345f5c93b3c3bbb96791028
GabrielGC <goncalves.cunhaa@gmail.com>
2024-10-21 11:02:49 -0300

    feat: adiciona logica para os resultados, exelente, bom, esquecido e amenésia.

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,31 +1,41 @@
-import classNames from 'classnames';
-import { useJogoDaMemoria } from '../hooks/useJogoDaMemoria';
+import classNames from "classnames";
+import { useJogoDaMemoria } from "../hooks/useJogoDaMemoria";
+import { useMemo } from "react";
+import { resultados } from "../constants/resultados";
 
 export const Resultado = () => {
+  const { idsDosParesEncontrados, cartas, quantidadeDeCartasViradas } =
+    useJogoDaMemoria();
 
-const { idsDosParesEncontrados, cartas } = useJogoDaMemoria();
+  const jogoFinalizou = cartas.length === idsDosParesEncontrados.length * 2;
 
-const jogoFinalizou = cartas.length === idsDosParesEncontrados.length * 2;
-
-  const cn = classNames('resultado',{
-    'resultado__aberto': jogoFinalizou
+  const cn = classNames("resultado", {
+    resultado__aberto: jogoFinalizou,
   });
+
+  const taxaDeAcertos = (cartas.length / quantidadeDeCartasViradas) * 100;
+  const resultado = useMemo(() => {
+    return resultados.find(({ min }) => min < taxaDeAcertos);
+  }, [taxaDeAcertos]);
+
   return (
     <div className={cn}>
       <div className="resultado__box">
         <p>Se nível de memória é:</p>
-        <h1>Bom</h1>
-        <img src="/monkey-3.png" alt="Image referente ao nível de memória" height={150} />
+        <h1>{resultado?.titulo}</h1>
+        <img
+          src={resultado?.imagem}
+          alt="Image referente ao nível de memória"
+          height={150}
+        />
         <p>
           <strong>Taxa de acertos: </strong>
-          <span>60%</span>
+          <span>{taxaDeAcertos.toFixed(0)}%</span>
         </p>
-        <button className="button">
-                    Nova Partida
-        </button>
+        <button className="button">Nova Partida</button>
         <p>
           <small>
-                        * Essa análise é ilustrativa e não possui base científica.
+            * Essa análise é ilustrativa e não possui base científica.
           </small>
         </p>
       </div>
---
+++ src/constants/resultados.js
@@ -0,0 +1,22 @@
+export const resultados = [
+    {
+        titulo: "Exelente",
+        imagem: "monkey-4.png",
+        min: 75,
+    },
+    {
+        titulo: "Bom",
+        imagem: "monkey-3.png",
+        min: 50,
+    },
+    {
+        titulo: "Esquecico",
+        imagem: "monkey-2.png",
+        min: 25,
+    },
+    {
+        titulo: "Amnésia",
+        imagem: "monkey-1.png",
+        min: 0,
+    },
+];
